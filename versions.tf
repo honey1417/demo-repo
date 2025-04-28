0886c929bdac98fcaf7297e4a893f23386a42176
harshini <harshinipamarthi@gmail.com>
2025-04-28 20:31:17 +0530

    c1

--- versions.tf
+++ versions.tf
@@ -1,7 +1,7 @@
 # Terraform Settings Block
 
 terraform {
-  required_version = ">= 1.5.7"
+  required_version = ">= 1.5.6"
   required_providers {
     google = {
       source = "hashicorp/google"
