880fed70a66975eb8d3e983e73e09be00e3b45e0
harshini <harshinipamarthi@gmail.com>
2025-04-29 14:13:13 +0530

    kubeconfig-remove

--- delegate.tf
+++ delegate.tf
@@ -17,7 +17,6 @@ provider "kubernetes" {
 # Helm Provider using GKE authentication
 provider "helm" {
   kubernetes {
-    config_path = "~/.kube/config"
     host                   = "https://${data.google_container_cluster.gke_cluster.endpoint}"
     token                  = data.google_client_config.default.access_token
     cluster_ca_certificate = base64decode(data.google_container_cluster.gke_cluster.master_auth[0].cluster_ca_certificate)
